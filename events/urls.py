b7b7721d557b022690b8f09a6d734b69b2485b31
doctor.madbee@gmail.com <Password@12>
2021-06-13 11:16:51 +0700

    save venues list to csv file

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('<int:year>/<str:month>/', views.home, name='home'),
     path('events_list/', views.events_list, name='events_list'),
-    ##path('venue/<int:event_id>', views.event_details, name='event_details'),
     path('event_add/', views.event_add, name='event_add'),
     path('event_upd/<int:event_id>', views.event_upd, name='event_upd'),
     path('event_del/<int:event_id>', views.event_del, name='event_del'),
@@ -14,6 +13,8 @@ urlpatterns = [
     path('venue_add/', views.venue_add, name='venue_add'),
     path('venue_upd/<int:venue_id>', views.venue_upd, name='venue_upd'),
     path('venue_del/<int:venue_id>', views.venue_del, name='venue_del'),
+    path('events_save_txt', views.events_save_txt, name='events_save_txt'),
+    path('events_save_csv', views.events_save_csv, name='events_save_csv'),
 ]
 
 
--- events/views.py
+++ events/views.py
@@ -1,8 +1,9 @@
 import calendar
 from datetime import datetime
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Event, Venue
 from .forms import VenueForm, EventForm
+import csv
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
@@ -79,9 +80,8 @@ def venue_del(request, venue_id):
     return redirect('venues_list')
 
 
-
 def events_list(request):
-    event_list = Event.objects.all()
+    event_list = Event.objects.all().order_by('name')
     next_month = list(calendar.month_name)[datetime.now().month + 1]
     return render(request, 'events/events_list.html', {
         'event_list': event_list,
@@ -124,3 +124,22 @@ def event_del(request, venue_id):
     event.delete()
     return redirect('events_list')
 
+
+def events_save_txt(request):
+    events = Event.objects.all()
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="events.txt"'
+    for i in events:
+        response.writelines(f'{i.name}\n{i.venue}\n{i.event_date}\n{i.event_time}\n\n\n')
+    return response
+
+
+def events_save_csv(request):
+    events = Event.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="events.csv"'
+    writer = csv.writer(response, delimiter='\t')
+    writer.writerow(['Name', 'Venue', 'Date', 'Time'])
+    for i in events:
+        writer.writerow([i.name, i.venue, i.event_date, i.event_time])
+    return response
